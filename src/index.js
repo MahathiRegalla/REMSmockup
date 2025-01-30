4a1f80ef6d6eb8452a4231aa148e02140d335322
MahathiRegalla <mahathiregalla1609@gmail.com>
2025-01-31 00:05:54 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RenewableEnergyManagementSystems from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RenewableEnergyManagementSystems />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
